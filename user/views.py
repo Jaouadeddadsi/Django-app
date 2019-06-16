069b23f6fc5a4a6d9a9d72ba10ebb9116861cc0a
Eddadsi <jaouadeddadsi2016@gmail.com>
2019-06-16 11:18:57 +0200

    Add Login page

--- user/views.py
+++ user/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 
@@ -8,12 +9,17 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'''Your account has been created!
+                             Your are now able to log in''')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, "user/register.html", {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'user/profile.html')
 
 
 # message.debug
